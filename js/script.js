853d3a4c60bc16a86a6fb429b5e1bcd0d41592b9
BakedBread03 <bepiskola@gmail.com>
2025-05-13 19:50:56 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@ const footer = document.querySelector("footer");
 const span = document.createElement("span");
 span.className = "footer-date";
 const now = new Date();
-span.textContent = `© 2025-${now.getFullYear()} | ${now.toLocaleTimeString()}`;
+span.textContent = `-${now.getFullYear()} | ${now.toLocaleTimeString()}`;
 footer.appendChild(span);
